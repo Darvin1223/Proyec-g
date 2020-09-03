9046efc5023fe144f971398e05050e77d86361d0
Gabriel Valero <ninjagabo131280@gmail.com>
2020-09-02 21:00:40 -0400

    Arreglo del javascript para el carrucel

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
+$(document).ready(function(){
+	console.log('Hola');
+	$('#click3').click(function(){
+		pasarimg($('#img3').attr('src'))
+	});
+	$('#click1').click(function(){
+		pasarimg($('#img1').attr('src'))
+	});
+	$('#click2').click(function(){
+		pasarimg($('#img2').attr('src'))
+	});
+})
+
+
 console.log('Ahi ahi');
+
 function pasarimg(src){
-	console.log(src);
+	console.log(src, 'Hola');
 	document.getElementById('img-principal').setAttribute("src", src);
 }
